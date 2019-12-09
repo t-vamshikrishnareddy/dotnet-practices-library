2186b595744a306244168320188e7f88c868fd3f
Wang Yucai <wangyucai@cheyipai.com>
2019-12-09 13:36:50 +0800

    feat(Practices.Library.Common.csproj): 新增性别枚举类型 src/Practices.Library.Common/Objects/Gender.cs

---
+++ src/Practices.Library.Common/Objects/Gender.cs
@@ -0,0 +1,41 @@
+﻿/*
+*******************************************************************************************************************************************************
+* MIT License
+*
+* Copyright © 2006 - 2019 Wang Yucai. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*******************************************************************************************************************************************************
+*/
+
+namespace NetowlsStudio.Practices.FoundationLibrary.Common.Objects
+{
+    /// <summary>定义了性别的枚举类型。</summary>
+    public enum Gender
+    {
+        /// <summary>男性。</summary>
+        Male = 0x4D,
+
+        /// <summary>女性。</summary>
+        Female = 0x46,
+
+        /// <summary>未知的或未提供的。</summary>
+        Unknown = 0x55
+    }
+}
