eaffbd75b51d89ebb5bbdb13050896cccbe20eb4
Wang Yucai <wangyucai@cheyipai.com>
2019-12-09 13:06:08 +0800

    feat(Practices.Library.Common.csproj): 新增字符串类型的标识名称接口 src/Practices.Library.Common/Objects/IStringKey.cs
    - Changes
        - Modify: src/Practices.Library.Common/Objects/IKey.cs
            格式化了代码。
        - Add: src/Practices.Library.Common/Objects/IStringKey.cs

--- src/Practices.Library.Common/Objects/IStringKey.cs
+++ src/Practices.Library.Common/Objects/IStringKey.cs
@@ -26,12 +26,9 @@
 
 namespace NetowlsStudio.Practices.FoundationLibrary.Common.Objects
 {
-    /// <summary>定义了标识名称接口。</summary>
-    /// <typeparam name="T">标识名称类型。</typeparam>
-    public interface IKey<T>
+    /// <summary>定义了 <see cref="string"/> 类型的标识名称接口。</summary>
+    /// <seealso cref="IKey{T}"/>
+    public interface IStringKey : IKey<string>
     {
-        /// <summary><typeparamref name="T"/> 类型的标识名称。</summary>
-        /// <value>设置或获取 <typeparamref name="T"/> 类型的对象实例或值，用于表示标识名称。</value>
-        T Key { get; set; }
     }
 }
